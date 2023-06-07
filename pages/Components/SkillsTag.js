a412ac552dedd7b9e8d1a2ff851114fb254a13e4
David-McCaig <davidmccaig1@gmail.com>
2023-06-07 18:04:08 +1200

    Create SkillsTage component. Create about and skills section

---
+++ pages/Components/SkillsTag.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SkillsTag({skill}) {
+    return (
+        <div className="flex justify-center mt-4 mr-4 h-11 w-24 overflow-hidden rounded-lg bg-white text-lg drop-shadow  ">
+            <h3 className="mt-2 text-gray-500 dark:text-black">{skill}</h3>
+        </div>
+    )
+}
+
+export default SkillsTag
