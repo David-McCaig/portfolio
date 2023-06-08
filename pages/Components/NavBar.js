1d3c2dfee62aacd49007eb43bef9cff6e899a127
David-McCaig <davidmccaig1@gmail.com>
2023-06-08 18:14:32 +1200

    Create nav bar

---
+++ pages/Components/NavBar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon, } from '@heroicons/react/24/outline'
+
+function NavBar({setDarkMode}) {
+
+    const navigation = [
+        { name: 'Bikes', href: '/bikes', current: false },
+        { name: 'Components', href: '/components', current: false },
+        { name: 'Post Add', href: '/upload', current: false },
+    ]
+
+  return (
+    <>
+    <nav className="pt-8 dark:text-white">
+    <div className="flex backdrop-blur-sm space-x-3 md:space-x-16 bg-white opacity-80  border-2 border-slate-100 rounded-full items-center pr-8 pl-8 h-14 dark:bg-black dark:border-2 dark:border-black ">
+    <a>About Me</a>
+    <a>Projects</a>
+    <a>Contact</a>
+    <div className=''>
+    <BsFillMoonStarsFill
+          onClick={() => setDarkMode( prevDarkMode=> !prevDarkMode)}
+          className=" cursor-pointer text-2xl"
+        />
+    </div>
+    </div>
+    {/* <ul className="flex items-center"> */}
+      {/* <li className='ml-5'>
+        <BsFillMoonStarsFill
+          onClick={() => setDarkMode(!darkMode)}
+          className=" cursor-pointer text-2xl"
+        />
+      </li> */}
+    {/* </ul> */}
+  </nav>
+  </>
+
+  
+  )
+}
+
+export default NavBar
