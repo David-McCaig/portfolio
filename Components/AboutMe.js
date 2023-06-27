78aa6ca89476a38d5c2521e5c5bb48e8f8eb5789
David-McCaig <davidmccaig1@gmail.com>
2023-06-27 20:30:23 +1200

    Add IntroSection and AboutMe components. Move code from index.js page into these components

---
+++ Components/AboutMe.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import SkillsTag from './SkillsTag'
+
+function AboutMe() {
+
+    return (
+
+        <section className="mb-24 lg:flex lg:mb-48 lg:max-w-7xl lg:mx-auto">
+            <div className="lg:w-2/3 mlax-w-2x">
+                <h3 className="text-3xl py-1 text-center lg:text-left  dark:text-white ">About Me</h3>
+                <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+                    I am a bike shop owner turned Web Developer with a passion for creating beautiful UI. I am an analytical problem solver at heart, with 8 years of experience working collaboratively to overcome challenges and build an inclusive and thriving business. I am excited to leverage my passion for technical problem solving, attention to detail and creative mind towards writing clean and efficient code. When I&#39;m not coding you can usually find me mountain biking in the Port Hills or making music with my collection of Synthesizers and drum machines.
+                </p>
+            </div>
+            <div className="md:w-16"></div>
+
+            <div className="lg:w-2/3 mt-0 md:py-8 lg:py-0 md:w-[37.5rem] m-auto ">
+                <h3 className="text-3xl py-1 justify-center lg:ml-1 lg:justify-start dark:text-white flex ">My Skills</h3>
+                <div className=" flex flex-wrap justify-center md:m-auto  md:justify-start">
+                    <SkillsTag skill={'HTML'} />
+                    <SkillsTag skill={'CSS'} />
+                    <SkillsTag skill={'JavaScript'} />
+                    <SkillsTag skill={'React'} />
+                    <SkillsTag skill={'NextJS'} />
+                    <SkillsTag skill={'SASS'} />
+                    <SkillsTag skill={'GIT'} />
+                    <SkillsTag skill={'Github'} />
+                    <SkillsTag skill={'Gitflow'} />
+                    <SkillsTag skill={'JIRA'} />
+                    <SkillsTag skill={'Agile'} />
+                    <SkillsTag skill={'Node.JS'} />
+                    <SkillsTag skill={'Express.JS'} />
+                    <SkillsTag skill={'MYSQL'} />
+                    <SkillsTag skill={'KNEX'} />
+                    <SkillsTag skill={'Socket.IO'} />
+                    <SkillsTag skill={'Jest'} />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AboutMe
---
+++ Components/IntroSection.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {
+    AiFillLinkedin,
+    AiFillGithub,
+  } from "react-icons/ai";
+import profilePhoto from "../public/david-mccaig.png";
+import Image from "next/image";
+
+function IntroSection({isActive, handleClick, handleMouseLeave, aboutMeRef}) {
+  
+    return (
+    <section className="mb-6 md:mb-0 ">
+
+    <div className="text-center  md:py-10">
+      <h2 className="text-5xl py-2 pt-12 md:pt-6 text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-emerald-300 dark:text-emerald-400 md:text-6xl">
+        David McCaig
+      </h2>
+      <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+        Full-Stack Developer
+      </h3>
+      <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+        I&#39;m a Full-Stack Developer with a love for creating beautiful UI and user-friendly applications.
+      </p>
+      <div className="text-5xl flex justify-center gap-16 pt-3  text-gray-600  dark:text-gray-400">
+        <a
+          className={isActive ? "" : "hover:text-emerald-600 transition duration-300 ease-in-out"}
+          onClick={handleClick}
+          onMouseLeave={handleMouseLeave}
+          href="https://www.linkedin.com/in/david-mccaig/"
+          target="_blank"
+          rel="noreferrer">
+          <AiFillLinkedin />
+        </a>
+        <a
+          className={ isActive ? "" : "hover:text-emerald-600 transition duration-300 ease-in-out"}
+          onClick={handleClick}
+          onMouseLeave={handleMouseLeave}
+          href="https://github.com/David-McCaig"
+          target="_blank"
+          rel="noreferrer">
+          <AiFillGithub />
+        </a>
+      </div>
+      <div ref={aboutMeRef} className="w-40 h-40 mx-auto bg-gradient-to-b from-teal-500 rounded-full relative overflow-hidden mt-8 md:mt-10 md:h-96 md:w-96">
+        <Image src={profilePhoto} alt={'project displayed'} layout="fill" objectFit="cover" />
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default IntroSection
