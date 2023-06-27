7ee936f809e5b2dc0acacb1ad323d19e4caa0eb6
David-McCaig <davidmccaig1@gmail.com>
2023-06-27 20:36:59 +1200

    Clean up order of styles in AboutMe

--- Components/AboutMe.js
+++ Components/AboutMe.js
@@ -6,7 +6,7 @@ function AboutMe() {
     return (
 
         <section className="mb-24 lg:flex lg:mb-48 lg:max-w-7xl lg:mx-auto">
-            <div className="lg:w-2/3 mlax-w-2x">
+            <div className="mlax-w-2x lg:w-2/3">
                 <h3 className="text-3xl py-1 text-center lg:text-left  dark:text-white ">About Me</h3>
                 <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
                     I am a bike shop owner turned Web Developer with a passion for creating beautiful UI. I am an analytical problem solver at heart, with 8 years of experience working collaboratively to overcome challenges and build an inclusive and thriving business. I am excited to leverage my passion for technical problem solving, attention to detail and creative mind towards writing clean and efficient code. When I&#39;m not coding you can usually find me mountain biking in the Port Hills or making music with my collection of Synthesizers and drum machines.
@@ -14,9 +14,9 @@ function AboutMe() {
             </div>
             <div className="md:w-16"></div>
 
-            <div className="lg:w-2/3 mt-0 md:py-8 lg:py-0 md:w-[37.5rem] m-auto ">
+            <div className="mt-0 md:py-8 lg:py-0 md:w-[37.5rem] m-auto lg:w-2/3">
                 <h3 className="text-3xl py-1 justify-center lg:ml-1 lg:justify-start dark:text-white flex ">My Skills</h3>
-                <div className=" flex flex-wrap justify-center md:m-auto  md:justify-start">
+                <div className="flex flex-wrap justify-center md:m-auto  md:justify-start">
                     <SkillsTag skill={'HTML'} />
                     <SkillsTag skill={'CSS'} />
                     <SkillsTag skill={'JavaScript'} />
